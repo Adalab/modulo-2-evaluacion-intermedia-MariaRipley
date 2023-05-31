ed613d99224c2d9520ee437ab919e733c39f0b48
MariaRipley <roblesb.maria@gmail.com>
2023-05-31 09:22:21 +0200

    Add classes, minimum styles and basic constants in js

--- js/main.js
+++ js/main.js
@@ -7,10 +7,17 @@ JS:
     1. Traer elementos del HTML necesarios
     2. Función para generar número aleatorio al cargar la página (llamarla FUERA de la función) --> console.log(nombreFunción(máx)) para q salga por consola el número generado
     3. Escuchar el evento click sobre el botón:
+        - preventDefault
         - Recoge value del select
         - Recoge value del input de apuesta
         - Los compara: 
             .si son iguales sustituye el contenido de h2 + multiplca*2 el input de apuesta + lo suma a la cantidad de span
             .si son diferentes sustituye el contenido de h2 + resta el input de apuesta a la cantidad de span
 */
+
+const selectNumber = document.querySelector('.js__select');
+const betInput = document.querySelector('.js__betInput');
+const btn = document.querySelector('.js__btn');
+const subtitle = document.querySelector('.js__subtitle');
+const remainBalance = document.querySelector('.js__remainBalance');
 
